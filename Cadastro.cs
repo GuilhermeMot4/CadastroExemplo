be3288b975754fdf54f688d1b350ce790e071f21
GuilhermeMot4 <32042163+GuilhermeMot4@users.noreply.github.com>
2018-06-05 13:57:36 -0300

    Add files via upload

---
+++ Cadastro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estoque
+{
+    public partial class Cadastro : Form
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //evento do botão
+        {
+            String query = "Insert into estoque_tb(nome,qtd,data) Values(@nome,@qtd,@data)"; //Consulta BD
+            SqlCommand cmd = new SqlCommand(query, conexao.conectar()); //Instância do comando
+            cmd.Parameters.AddWithValue("@nome", textBox1.Text);       //parâmetro nome
+            cmd.Parameters.AddWithValue("@qtd", textBox2.Text);        //parâmetro qtd
+            cmd.Parameters.AddWithValue("@data", maskedTextBox1.Text); //parâmetro data
+            cmd.ExecuteNonQuery(); //exexuta comando
+            MessageBox.Show("Cadastrado com sucesso"); //feedback
+        }
+    }
+}
---
+++ conexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    class conexao
+    {
+        public static SqlConnection conectar() { //método que abre uma conexão
+            String str = "Data Source=localhost;Initial Catalog=estoqueBD;Integrated Security=True"; //string de conexão
+            SqlConnection connection = new SqlConnection(str);
+            connection.Open(); //abre conexão
+            return connection; //retorna conexão criada
+        } 
+    }
+}
